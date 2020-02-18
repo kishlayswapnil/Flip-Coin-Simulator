f5afc35e864f7a56bfcf71a6161766e439ce4222
kishlayswapnil <kishlayswapnil@gmail.com>
2020-02-18 10:43:36 +0530

    Added: the singlet dictionary to store values of h and t is being done with percent.

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,13 +1,40 @@
 #!/bin/bash -x
 echo xxxxxxxxxxWELCOMExxxxTOxxxxFLIPxxxxCOINxxxxSIMULATORxxxxxxxxxxx
 
-#Declaring varriables.
-check=1
-random=$((RANDOM%2))
-#logic to check if head or tail is comming.
-if (( random == check ))
-then
-	echo HEAD
-else
-	echo TAIL
-fi
+#Constants declared.
+CHECK=1
+#Varriable declared.
+heads=0
+tails=0
+
+read -p "Enter the number you want to flip the coin:" range
+#declareing a dictionary to store the count
+declare -A singlet
+#logic for the operation.
+function flip() {
+	random=$((RANDOM%2))
+	echo $random
+}
+for (( count=0; count<range; count++))
+do
+	result="$(flip)"
+	if (( $result == $CHECK  ))
+	then
+ 		echo "HEAD"
+		singlet[$count]=H
+		((heads++))
+	else
+		echo "TAIL"
+		singlet[$count]=T
+		((tails++))
+	fi
+done
+echo "Heads count= $heads"
+echo "Tails count= $tails"
+echo ${singlet[@]}
+
+#calculating the percentage of heads and tails
+headPercent=`echo "scale=4;($heads/$range)*100" | bc`"%"
+	echo "pecentage of heads is: $headPercent"
+tailPercent=`echo "scale=4;($tails/$range)*100" | bc`"%"
+	echo "pecentage of tails is: $tailPercent"
